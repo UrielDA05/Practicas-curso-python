4ddc1735489a4dcfd12acfb87f120ff47c70c71a
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-11-01 22:51:10 -0600

    inicio

--- nivel principiante/hello world.py
+++
@@ -1,7 +0,0 @@
-# Esto es un comentorio
-# Mi hola mundo en python
-print ("Hola Python");
-# Hola mundo con suma de variables
-variable1 = "Hola "
-variable2 = "Mundo"
-print( variable1 + variable2 + "!" );
