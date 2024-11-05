82f7beabe0fde71f4ab5939ae3ea840cb6b0ad03
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-11-04 21:23:34 -0600

    concatenacion

--- nivel principiante/hola mundo.py
+++ nivel principiante/hola mundo.py
@@ -1,2 +1,9 @@
 print ("Hello world");
 print ("Mi nombre es Uriel")
+# concatenacion de cadenas
+nombre = "Uriel"
+apellido = "De jesus"
+print (nombre)
+print (apellido)
+nombreCompleto = (nombre + " " + apellido)
+print (nombreCompleto)
