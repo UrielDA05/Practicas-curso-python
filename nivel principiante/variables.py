fca2c54c6d6069631e5961c0bbcd1c68f6c1180d
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-11-11 22:20:29 -0600

    tipos de variables

---
+++ nivel principiante/variables.py
@@ -0,0 +1,15 @@
+#valores numericos
+numero=10.56
+print (numero)
+# Define el tipo de dato de la variable
+print(type(numero))
+
+#imprimir texto entre comillas
+texto=("Estudio 'mucho'")
+print (texto)
+print (type(texto))
+#Valores booleanos
+
+valor =False
+print(type(valor))
+print(valor)
