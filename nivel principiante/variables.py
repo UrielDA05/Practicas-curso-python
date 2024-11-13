2e071e461c0445c6c63fec1c320f77f90db01245
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-11-12 20:39:21 -0600

    tipos de variables

--- nivel principiante/variables.py
+++ nivel principiante/variables.py
@@ -8,8 +8,20 @@ print(type(numero))
 texto=("Estudio 'mucho'")
 print (texto)
 print (type(texto))
-#Valores booleanos
 
+#Valores booleanos
 valor =False
-print(type(valor))
 print(valor)
+print(type(valor))
+
+#Ejemplo de suma
+num1=23.54
+num2=14.06
+resultado=num1+num2
+print(resultado)
+
+#Tipado dinamico
+dato=15
+print(dato)
+dato="Pablo"
+print(dato)
